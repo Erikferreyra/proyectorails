11470e1aaabe06f8c9ce4327d42cfc12fb3be4d8
Agustin Rampinini <rampininiagustin@gmail.com>
2018-11-12 18:24:04 -0300

    Modificacion root

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :subastas
   resources :homes
   get 'adm',to: 'adm#index'
-  root 'main#index'
+  root 'adm#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
