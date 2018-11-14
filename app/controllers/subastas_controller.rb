5c312b9f39f18535931c2685023e38ff913dc311
ivan <20ivansandoval@gmail.com>
2018-11-14 16:30:18 -0300

    actualizacion

--- app/controllers/subastas_controller.rb
+++ app/controllers/subastas_controller.rb
@@ -1,30 +1,31 @@
 class SubastasController < ApplicationController
   def index
+  	 @reservas=Reservation.all
   	 @subastas=Subasta.all
-     @reservas=Reservation.all
-     t=Time.new
-     if t.wday==3 
-        f = Date.new t.year,t.month,t.day 
-        @reservas.each do |res| 
+  	 t=Time.new
+	   if t.wday==3
+      f = Date.new t.year,t.month,t.day 
+		 @reservas.each do |res|
           res.fecha_ini= f >> 6
           res.save  
-          create(res.nombre,res.id,f)
-        end
-      end
-      redirect_to url_for(action: 'index',controller: 'homes')
+	        create(res.nombre,res.id,f)
+	     end
+	   end
   end
-   def new 
-    @subastas = Subasta.new
+
+
+   def new
+    @subasta = Subasta.new
    end
 
-   def create(nombre,id,f)
-   	 #@resi = params[:resi] 
-     @subastas=Subasta.new
-     @subastas.titulo = nombre
-     @subastas.reservation_id = id
-     @subastas.fechainicio = f 
-     @subastas.save
- 
-   end
-   
+end
+
+private
+def create(nombre,id,t)
+  #@resi = params[:resi]
+  @subasta=Subasta.new
+  @subasta.titulo = nombre
+  @subasta.reservation_id = id
+  @subasta.fechainicio = t >> 6
+  @subasta.save
 end
