9df434162df5d4d8bd674271740829988e3cbacf
ivan <20ivansandoval@gmail.com>
2018-11-26 00:01:08 -0300

    mejorando las vistas de ingreso y registro

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   resources :subastas
   resources :homes
   get 'adm',to: 'adm#index'
-  root 'adm#index'
+  devise_scope :user do
+      root to: "devise/sessions#new"
+  end
   #get 'create',to: 'subastas#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
