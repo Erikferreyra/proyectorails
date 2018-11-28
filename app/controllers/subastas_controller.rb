a7e1262fb6940ad2d95465a5547d6669aa2ebe81
ivan <20ivansandoval@gmail.com>
2018-11-28 15:43:29 -0300

    a

--- app/controllers/subastas_controller.rb
+++ app/controllers/subastas_controller.rb
@@ -2,17 +2,8 @@ class SubastasController < ApplicationController
     def index
     	 @reservas=Reservation.all
     	 @subastas=Subasta.all
-    	 t=Time.new
-
-  	   if t.wday==3
-        f = Date.new t.year,t.month,t.day
-    		 @reservas.each do |res|
-              res.fecha_ini= f >> 6
-              res.save
-    	        create(res.nombre,res.id,f)
-    	   end
-  	   end
-    end
+       @homes=Home.all
+	end
 
 
    def new
