a9fe8d3955a9625b659f6de825ae9e97b852edc3
Erik <coloferreyra05@gmail.com>
2018-12-04 17:22:51 -0300

    actualizacion

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,4 +1,5 @@
 class Home < ApplicationRecord
  has_one :reservation
- validates :nombre, :ubicacion, :canthabitacion, presence: true 
+ validates :nombre, :ciudad,:direccion, :canthabitacion, presence: true
+ validates :nombre, uniqueness:true 
 end
