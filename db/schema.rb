b62b34b25fe4f131c66a614d161dd26a0e6fc83e
ivan <20ivansandoval@gmail.com>
2018-12-04 14:26:31 -0300

    a

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20181203030244) do
 
   create_table "homes", force: :cascade do |t|
     t.string "nombre"
+    t.string "ubicacion"
     t.integer "canthabitacion"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
