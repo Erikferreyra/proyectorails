73a096e6f327fa60558433ddc269763955ddaf01
ivan <20ivansandoval@gmail.com>
2018-12-04 15:00:04 -0300

    a

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,5 @@
 class Home < ApplicationRecord
  has_one :reservation
  validates :nombre, :ciudad, :direccion, :canthabitacion, presence: true 
- validates :fecha_ini, uniqueness: true
+ validates :id_reservation, uniqueness: true
 end
