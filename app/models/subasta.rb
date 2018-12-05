d12cd9b8308cc5de05132632526e1db83a21d0e6
ivan <20ivansandoval@gmail.com>
2018-12-04 22:57:13 -0300

    actualizacion

--- app/models/subasta.rb
+++ app/models/subasta.rb
@@ -1,3 +1,4 @@
 class Subasta < ApplicationRecord
   has_many :reservation
+  validates :id_reservation, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nombre,:apellido,:fechanac, presence: true
+  validates :nombre,:apellido,:fechanac,presence: true
+  validates :numtarjeta, presence: true,numericality: { only_integer: true },length: { minimum:16,maximum:17 }
 end
