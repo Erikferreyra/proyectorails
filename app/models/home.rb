5c1fb64dea9fca21b8d0c7bad4c4809bda2aee1b
ivan <20ivansandoval@gmail.com>
2018-12-05 00:48:50 -0300

    actualizacion

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,5 @@
 class Home < ApplicationRecord
  has_one :reservation
  validates :nombre, :ciudad, :direccion, :canthabitacion, presence: true 
- validates :nombre, :uniqueness =>{ message: "No puede dejarse vacío" }
+ validates :nombre, uniqueness: { scope: :year,message: "should happen once per year" }
 end
