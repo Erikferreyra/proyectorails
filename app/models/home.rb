361e24fc8f800fdc84a5e73b36ad2af369f87438
ivan <20ivansandoval@gmail.com>
2018-12-05 00:58:59 -0300

    actualizacion

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,5 @@
 class Home < ApplicationRecord
  has_one :reservation
  validates :nombre, :ciudad, :direccion, :canthabitacion, presence: true 
- validates :nombre, uniqueness: { scope: :year,message: "should happen once per year" }
+ validates :nombre, uniqueness: true
 end
