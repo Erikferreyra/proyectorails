f3741e3859f534cfbe815eb4dde4d36329031672
ivan <20ivansandoval@gmail.com>
2018-12-05 00:34:18 -0300

    actualizacion

--- app/models/home.rb
+++ app/models/home.rb
@@ -1,5 +1,5 @@
 class Home < ApplicationRecord
  has_one :reservation
  validates :nombre, :ciudad, :direccion, :canthabitacion, presence: true 
- validates :nombre, uniqueness: true,message: "El nombre de la residencia ya existee" 
+ validates :nombre, uniqueness: true {message: "should happen once per year" }
 end
