7dfa5ce6e44661b0daf024390091cf7c21021adf
ruussiittoo <rampininiagustin@gmail.com>
2018-12-08 21:50:46 -0300

    asd

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -17,13 +17,18 @@ class ReservationsController < ApplicationController
     @reserva=Reservation.find(params[:id])
 
     @user = current_user
-		@user.creditos -= 1
 
-    if (@reserva.update reserva_params) && (@user.save)
-      redirect_to subastas_path, notice: "Se adjudico la reserva. Creditos restantes: #{@user.creditos}"
-    else
-      redirect_to subastas_path, notice: "No se pudo adjudicar la reserva"
-    end
+		if(@user.creditos <= 0)
+			redirect_to subastas_path, alert: "No dispones de creditos!"
+		else
+			@user.creditos -= 1
+
+	    if (@reserva.update reserva_params) && (@user.save) && (@user.try(:tipousuario?))
+	      redirect_to subastas_path, notice: "Se adjudico la reserva. Creditos restantes: #{@user.creditos}"
+	    else
+	      redirect_to subastas_path, notice: "No se pudo adjudicar la reserva"
+	    end
+		end
   end
 
   def reserva_params
